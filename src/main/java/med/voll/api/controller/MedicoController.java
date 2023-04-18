3cdd512d815e77b119cc77bb3bf5a0d3a15ca388
Karleia <karleia.c.o.s@gmail.com>
2023-04-18 17:30:51 -0300

    Anotação @PageableDfault com parametros nas Controllers - Config exib query applicaiton.properties

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,11 +20,10 @@ public class MedicoController {
     @PostMapping
     @Transactional
     public void register(@RequestBody @Valid DadosCadastroMedico dados) {
-
         repository.save(new Medico(dados));
     }
     @GetMapping
-    public Page<DadosListagemMedicos> listar(Pageable paginacao) {
+    public Page<DadosListagemMedicos> listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao) {
        return repository.findAll(paginacao).map(DadosListagemMedicos::new);
     }
 }
--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -8,6 +8,7 @@ import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +23,7 @@ public class PacienteController {
         repository.save(new Paciente(dados));
     }
     @GetMapping
-    public Page<DadosListagemPacientes> listar(Pageable paginacao) {
+    public Page<DadosListagemPacientes> listar(@PageableDefault(size = 10, sort = "nome") Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemPacientes::new);
     }
 }
